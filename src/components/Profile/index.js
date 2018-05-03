6d49b8f8c6d796e7a15017e4de967d201f19ebf1
Ryan Yogan <ryanyogan@gmail.com>
2018-05-03 09:49:47 -0700

    Moving large queries to fragments

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
 import Loading from '../Loading';
-import RepositoryList from '../Repository';
+import RepositoryList, { REPOSITORY_FRAGMENT } from '../Repository';
 import ErrorMessage from '../Error';
 
 const GET_REPOS_OF_CURRENT_USER = gql`
@@ -12,30 +12,14 @@ const GET_REPOS_OF_CURRENT_USER = gql`
       repositories(first: 5, orderBy: { direction: DESC, field: STARGAZERS }) {
         edges {
           node {
-            id
-            name
-            url
-            descriptionHTML
-            primaryLanguage {
-              name
-            }
-            owner {
-              login
-              url
-            }
-            stargazers {
-              totalCount
-            }
-            viewerHasStarred
-            watchers {
-              totalCount
-            }
-            viewerSubscription
+            ...repository
           }
         }
       }
     }
   }
+
+  ${REPOSITORY_FRAGMENT}
 `;
 
 const Profile = () => (
